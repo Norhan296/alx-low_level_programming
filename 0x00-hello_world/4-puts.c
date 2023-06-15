899c2db8f4148f1abbb0fc7c696134beb54ed480
Norhan296 <norhanshawkat296@gmail.com>
2023-06-15 15:19:29 -0700

    mkd

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - a program to print a line using puts function
+ * Return: 0 (success)
+ */
+
+
+int main(void)
+{
+
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
