28919255f8f45513d38fbdbe05f4a67d6cd77268
Norhan296 <norhanshawkat296@gmail.com>
2023-06-16 22:20:56 -0700

    as

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a = 0;
+
+	while
+		(a < 10) {
+			printf("%d", a);
+			a++;
+		}
+	return (0);
+}
