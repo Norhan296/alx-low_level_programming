9c604520c3ed462b1c421efbf66ca1badda5cefb
Norhan296 <norhanshawkat296@gmail.com>
2023-06-17 02:37:42 -0700

    as

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a = 0;
+	char s = 'a';
+
+	while
+		(a <= 9) {
+		putchar(a + 48);
+		a++;
+		}
+	while
+		(s <= 'f') {
+		putchar(s);
+	        s++;
+		}
+	putchar('\n');
+	return (0);
+}
