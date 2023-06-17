b59f555c40fcc2de36f5057905657f71f1f3a2a0
Norhan296 <norhanshawkat296@gmail.com>
2023-06-17 01:25:48 -0700

    as

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char a;
+
+	a = 'z';
+	while
+		(a <= 'z') {
+			putchar(a);
+			a--;
+		}
+	putchar('\n');
+	return (0);
+
+}
