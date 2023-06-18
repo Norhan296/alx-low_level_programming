26b4521830969f7a4930592dafe690a450538d50
Norhan296 <norhanshawkat296@gmail.com>
2023-06-18 00:09:24 -0700

    asd

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,29 +11,29 @@ int main(void)
 
 	while
 		(a <= 9) {
-			b = 0;
+		b = 0;
+		while
+			(b <= 9) {
+			c = 0;
 			while
-				(b <= 9) {
-					c = 0;
-					while
-						(c <= 9) {
-							if (a != b && a < b && b != c && b < c)
-							{
-								putchar(a + 48);
-								putchar(b + 48);
-								putchar(c + 48);
-								if (a + b + c != 24)
-								{
-									putchar(',');
-									putchar(' ');
-								}
-							}
-							c++;
-						}
-					b++;
+				(c <= 9) {
+				if (a != b && a < b && b != c && b < c)
+				{
+					putchar(a + 48);
+					putchar(b + 48);
+				        putchar(c + 48);
+					if (a + b + c != 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
 				}
-			a++;
+				c++;
+			}
+			b++;
 		}
+		a++;
+	}
 	putchar('\n');
 	return (0);
 }
