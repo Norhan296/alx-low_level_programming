64b910a5b10fbc036645e158e672dd25ec3d4d54
Norhan296 <norhanshawkat296@gmail.com>
2023-06-17 23:47:51 -0700

    lkj

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a = 0;
+	int b, c;
+
+	while
+		(a <= 9) {
+			b = 0;
+			while
+				(b <= 9) {
+					c = 0;
+					while
+						(c <= 9) {
+							if (a != b && a < b && b != c && b < c)
+							{
+								putchar(a + 48);
+								putchar(b + 48);
+								putchar(c + 48);
+								if (a + b + c != 24)
+								{
+									putchar(',');
+									putchar(' ');
+								}
+							}
+							c++;
+						}
+					b++;
+				}
+			a++;
+		}
+	putchar('\n');
+	return (0);
+}
+
