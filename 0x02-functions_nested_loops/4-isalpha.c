23e7c7732a7201ffd29b6bf442fbf1f34f09f6ce
Norhan296 <norhanshawkat296@gmail.com>
2023-06-21 03:05:40 -0700

    as

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _is alpha - function to chech if c is letter
+ * in lowercase or uppercase
+ *
+ * Return: return 1 if it is letter
+ * and retrun 0 if not
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	if (c >= 97 && c <= 122)
+	       return (1);
+	return (0);
+}
