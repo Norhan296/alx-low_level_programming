e18e57dda220234cd8a65e3f93588e27ece537ef
Norhan296 <norhanshawkat296@gmail.com>
2023-06-21 08:26:15 -0700

    lk

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -22,7 +22,7 @@ void times_table(void)
 				_putchar(' ');
 			else
 				_putchar((prod / 10) + 48);
-		                _putchar((prod % 10) + 48);
+		        _putchar((prod % 10) + 48);
 		}
 		_putchar('\n');
 	}
