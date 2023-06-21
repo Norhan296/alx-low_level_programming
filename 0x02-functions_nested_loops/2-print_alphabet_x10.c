06c7980db57cb39c1c884d433f78fe7dd593c3e5
Norhan296 <norhanshawkat296@gmail.com>
2023-06-21 02:25:32 -0700

    lk

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,7 +7,7 @@
 void print_alphabet_x10(void)
 {
 	int line. c;
-	for (line = 0; line <= 10; line++)
+	for (line = 1; line <= 10; line++)
 		for(c = 'a'; c <= 'z'; c++)
 			_putchar(c);
 	_putchar('\n');
