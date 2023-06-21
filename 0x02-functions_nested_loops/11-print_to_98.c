c67b03db8cdf8eb4df980175b85ef7c982b3ecf0
Norhan296 <norhanshawkat296@gmail.com>
2023-06-21 09:04:59 -0700

    as

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,10 +13,12 @@ void print_to_98(int n)
 	{
 		for (m = n; m < 98; m++)
 			printf("%d", m);
+		        printf(", ");
 	}
 	else if (n > 98)
 		for (m = n; m > 98; m--)
 			printf("%d", m);
+	                printf(", ");
 	else
 		printf("98\n");
 }
