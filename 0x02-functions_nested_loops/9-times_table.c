f54f4b7f92abe2cfd1d655d4f5fed7769d85a656
Norhan296 <norhanshawkat296@gmail.com>
2023-06-21 06:52:06 -0700

    lk

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,7 +11,7 @@ void times_table(void)
 
 	for (num = 0; num <= 9; num++)
 	{
-		putchar(48);
+		putchar('0');
 		for (mult = 1; mult <= 9; mult++)
 		{
 			_putchar(',');
@@ -21,7 +21,7 @@ void times_table(void)
 				_putchar(' ');
 			else
 				_putchar((prod / 10) + 48);
-			_putchar((prod % 10) + 48);
+			        _putchar((prod % 10) + 48);
 		}
 		_putchar('\n');
 	}
