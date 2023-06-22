6d90b99a27078d82d48d734f21dc0a84a75062d0
Norhan296 <norhanshawkat296@gmail.com>
2023-06-22 11:04:44 -0700

    kj

--- 0x03-debubgging/2-largest_number.c
+++ 0x03-debubgging/2-largest_number.c
@@ -1,10 +1,6 @@
 #include "main.h"
-
 /**
- * largest_number - returns the largest of 3 numbers
- * @a: first integer
- * @b: second integer
- * @c: third integer
+ * largest_number - returns the largest of 3 numbr
  * Return: largest number
  */
 
