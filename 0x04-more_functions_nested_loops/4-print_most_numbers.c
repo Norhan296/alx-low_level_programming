141f558f65ea9345e1840444decba30b7a021c75
Norhan296 <norhanshawkat296@gmail.com>
2023-06-23 01:20:16 -0700

    lk

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints from 0 - 9 
+ * Except 2 and 4
+ * Return: Always 0 (success)
+ */
+
+void print_most_numbers(void)
+{
+	int n = 0;
+
+	while (n >= 48 && n <= 57)
+	{
+		if (n == 2 || n == 4)
+			continue
+		else
+			_putchar(n + 48);
+	}
+	_putchar('\n');
+}
