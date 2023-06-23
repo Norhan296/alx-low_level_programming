1233469f30d352b5b9627039effed1b5e1e3f15d
Norhan296 <norhanshawkat296@gmail.com>
2023-06-23 13:01:04 -0700

    as

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,8 +10,10 @@ void print_number(int n)
 	unsigned int num = n;
 
 	if (n < 0)
+	{
 		_putchar('-');
 		num = -num;
+	}
 	if ((n / 10) >  0)
 		print_number(num / 10);
 	_putchar((num % 10) + 48);
