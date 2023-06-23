4d14753f370a1a29f1ca6f521c3820f49a55e93e
Norhan296 <norhanshawkat296@gmail.com>
2023-06-23 02:32:14 -0700

    lk

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,10 +10,14 @@
 
 void print_line(int n)
 {
+	int m;
+
 	if (n <= 0)
 		_putchar('\n');
 	else
-		for (m = 0; m = n; m++)
+	{
+		for (m = 1; m <= n; m++)
 			_putchar('_');
+	}
 	_putchar('\n');
 }
