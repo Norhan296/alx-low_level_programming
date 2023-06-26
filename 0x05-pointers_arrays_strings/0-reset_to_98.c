09e0bf3dfcdd602987625a538b9525eb3b6d7e4f
Norhan296 <norhanshawkat296@gmail.com>
2023-06-26 04:13:34 -0700

    kj

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int pareamter
+ *            and updates it to 98
+ * @n: int parameter
+ * Return: nothing
+ */
+
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * putchar write the character c to stdout
+ * @c: the character to print
+ * Return: on success 1
+ *         on error, -1 is returned. and error
+ */
+int _putchar(char c)
+{
+	reurn (write(1, &,1));
+}
