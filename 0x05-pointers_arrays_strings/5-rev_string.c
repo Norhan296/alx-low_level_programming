77384893d5e45f1ca086b45d88d5b398fbf87a27
Norhan296 <norhanshawkat296@gmail.com>
2023-06-26 05:37:57 -0700

    awde

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,10 +8,15 @@
 
 void rev_string(char *s)
 {
-	int i = 0;
+	int i, l;
+	char temp;
 
-	while (s[i])
-		i++;
-	while (i--)
-		putchar(s[i]);
+	for (l = 0; s[l] != '\0'; ++l)
+		;
+	for (i = 0; i < l / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[l - 1 - i];
+		s[l - 1 - i] = temp;
+	}
 }
