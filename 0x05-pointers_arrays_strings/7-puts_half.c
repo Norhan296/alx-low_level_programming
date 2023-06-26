c9f332002ffc1666597212463c36412d69e320a7
Norhan296 <norhanshawkat296@gmail.com>
2023-06-26 06:29:51 -0700

    asw

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts_half - prints the second half of the string
+ * @str: the string
+ * Return: nothing
+ */
+
+void puts_half(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	i++;
+	for (i /= 2; str[i] != '\0'; i++)
+	{
+		putchar(str[i]);
+	}
+	putchar('\n');
+}
