124c902e3cee9faeb4e26bf0cfd1784697cbeaf2
Norhan296 <norhanshawkat296@gmail.com>
2023-06-26 12:32:27 -0700

    lko

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,9 +1,25 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
- *
- *
- *
- *
+ * main - program that prints a random password
+ * Return: zero
  */
+
+int main(void)
+{
+	int sum;
+	char c;
+
+	srand(time(NULL));
+	while (sum <= 2645)
+	{
+		c = rand() % 128;
+		sum += c;
+		putchar(c);
+	}
+	putchar(2772 - sum);
+	return (0);
+}
 
