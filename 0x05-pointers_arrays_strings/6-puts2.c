29b0f04122d8b94d70a2a5cf22a8739d5d1d36db
Norhan296 <norhanshawkat296@gmail.com>
2023-06-26 05:53:21 -0700

    poiu

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *puts2 - prints every other char of a string
+ *
+ * @str: the string
+ * Return: nothing
+ */
+
+void puts2(char *str)
+{
+	int i;
+	
+	for (i = 0; str[i] != '\0'; ++i)
+	{
+		if (i % 2 == 0)
+			putchar(str[i]);
+	}
+	putchar('\n');
+}
