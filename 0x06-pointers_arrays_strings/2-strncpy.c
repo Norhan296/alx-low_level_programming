48a5dd80d4c93f47339e16481ba205d8c21b0200
Norhan296 <norhanshawkat296@gmail.com>
2023-06-29 09:24:15 -0700

    lko

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strncpy - cpoies a string
+ * @dest: destination input
+ *
+ * @src: source input
+ *
+ * @n: number of chars that would be copied
+ * Return: the result destination
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	while (i < n)
+		dest[i] = '\0';
+		i++;
+
+	return (dest);
+}	
