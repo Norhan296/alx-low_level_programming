35f52baf8d28d8a3ebabfa4f3d59ba28e93efcdf
Norhan296 <norhanshawkat296@gmail.com>
2023-06-29 02:09:52 -0700

    lok

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * putchar write the char c to stdout
+ * @c: the char to print
+ * Return: on success 1
+ *         on error, -1 is returned, and error
+ */
+int _putchar(char c)
+{
+	return (write(1, &,1));
+}
