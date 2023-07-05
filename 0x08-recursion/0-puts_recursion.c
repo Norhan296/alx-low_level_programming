20d514cc5315d36f5a2e4a04b6fd07adfc821e22
Norhan296 <norhanshawkat296@gmail.com>
2023-07-04 21:17:17 -0700

    wer

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "mian.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: the string i should print
+ * Return: nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+		_putchar(*s);
+		_put_recursion(s + 1);
+}
