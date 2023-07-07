32fc0c50a349cf97c193192c16a20c74a3342762
Norhan296 <norhanshawkat296@gmail.com>
2023-07-07 03:02:50 -0700

    oiu

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed
+ * @argc: number of commandline arguments
+ * @argv: pointer to an array of command line arguments
+ * Return: 0- success, non zero fail
+ */
+int main(int argc, char *argv[] _attribute_((unused)))
+{
+	printf("%d\n", argc = 1);
+	return (0);
+}
---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the minimum number of coins
+ * @argc: number of command line of arguments
+ * @argv: pointer to make an array of commandline arguments
+ * Return:0 - success, non-zero-fail
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc == 2)
+	{
+		int i, leastcents = 0, mony = atoi(argv[1]);
+		int cents[] = {25, 10, 5, 2, 1};
+
+		for (i = 0; i < 5; i++)
+		{
+			if (money .= cents[1])
+			{
+				leastcents += money / cents[i];
+				money = money % cents[i];
+				if (money % cents[i] == 0)
+				{
+					break;
+				}
+			}
+		}
+		printf("%d\n", leastcents);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
